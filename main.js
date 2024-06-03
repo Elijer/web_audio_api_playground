6a0e1de7ab09984a76ef4ce66b02f4465b3593eb
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-03 18:48:40 -0400

    moved the working stuff into a class

--- main.js
+++ main.js
@@ -1,81 +1,133 @@
-console.clear();
-
-// for cross browser
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
-
-// load some sound
-const audioElement = document.querySelector('audio');
-const track = audioCtx.createMediaElementSource(audioElement);
-
-const playButton = document.querySelector('.tape-controls-play');
-
-let soundPlaying = false
-let makePlay = false
-
-document.addEventListener('keydown', (event) => {
-
-  if (event.key === "f"){
-  //       // check if context is in suspended state (autoplay policy)
-    if (audioCtx.state === 'suspended') {
-      audioCtx.resume();
-    }
-    
-    if (soundPlaying === false) {
-      audioElement.play();
-      soundPlaying = true;
-    // if track is playing pause it
-    } else if (soundPlaying === true) {
-      audioElement.pause();
-      soundPlaying = false;
-    }
+class AudioEngine {
+  constructor() {
+    this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   }
+
+  armAudio(){
+
+    // load some sound
+    const audioElement = document.querySelector('audio');
+    const track = this.audioCtx.createMediaElementSource(audioElement);
+
+    const playButton = document.querySelector('.tape-controls-play');
+
+    let soundPlaying = false
+    let makePlay = false
+
+    document.addEventListener('keydown', (event) => {
+
+      if (event.key === "f"){
+      //       // check if context is in suspended state (autoplay policy)
+        if (this.audioCtx.state === 'suspended') {
+          this.audioCtx.resume();
+        }
+        
+        if (soundPlaying === false) {
+          audioElement.play();
+          soundPlaying = true;
+        // if track is playing pause it
+        } else if (soundPlaying === true) {
+          audioElement.pause();
+          soundPlaying = false;
+        }
+      }
+    })
+
+
+    // if track ends
+    audioElement.addEventListener('ended', () => {
+      playButton.dataset.playing = 'false';
+      playButton.setAttribute( "aria-checked", "false" );
+    }, false);
+
+    track.connect(this.audioCtx.destination);
+  }
+
+}
+
+const speakers = new AudioEngine();
+speakers.armAudio();
+
+
+// // load some sound
+// const audioElement = document.querySelector('audio');
+// const track = audioCtx.createMediaElementSource(audioElement);
+
+// const playButton = document.querySelector('.tape-controls-play');
+
+// let soundPlaying = false
+// let makePlay = false
+
+// document.addEventListener('keydown', (event) => {
+
+//   if (event.key === "f"){
+//   //       // check if context is in suspended state (autoplay policy)
+//     if (audioCtx.state === 'suspended') {
+//       audioCtx.resume();
+//     }
     
-    // let state = this.getAttribute('aria-checked') === "true" ? true : false;
-    // this.setAttribute( 'aria-checked', state ? "false" : "true" );
-  // }
-})
+//     if (soundPlaying === false) {
+//       audioElement.play();
+//       soundPlaying = true;
+//     // if track is playing pause it
+//     } else if (soundPlaying === true) {
+//       audioElement.pause();
+//       soundPlaying = false;
+//     }
+//   }
+// })
 
 
-// if track ends
-audioElement.addEventListener('ended', () => {
-	playButton.dataset.playing = 'false';
-	playButton.setAttribute( "aria-checked", "false" );
-}, false);
+// // if track ends
+// audioElement.addEventListener('ended', () => {
+// 	playButton.dataset.playing = 'false';
+// 	playButton.setAttribute( "aria-checked", "false" );
+// }, false);
 
 // volume
-const gainNode = audioCtx.createGain();
+// let gainValue = .01
+// let direction = 1
+// const gainNode = audioCtx.createGain();
 
-const volumeControl = document.querySelector('[data-action="volume"]');
-volumeControl.addEventListener('input', function() {
-	gainNode.gain.value = this.value;
-}, false);
+// setInterval(() => {
+//   console.log(gainValue)
+//   if (gainValue >= 10){
+//     direction = -1;
+//   } else if (gainValue <= 0){
+//     direction = 1
+//   }
+
+//   gainNode.gain.value = gainValue
+
+//   gainValue += direction * .01
+// }, 2000)
 
 // panning
-const pannerOptions = {pan: 0};
-const panner = new StereoPannerNode(audioCtx, pannerOptions);
+// const pannerOptions = {pan: 0};
+// const panner = new StereoPannerNode(audioCtx, pannerOptions);
 
-const pannerControl = document.querySelector('[data-action="panner"]');
-pannerControl.addEventListener('input', function() {
-	panner.pan.value = this.value;	
-}, false);
+// const pannerControl = document.querySelector('[data-action="panner"]');
+// pannerControl.addEventListener('input', function() {
+// 	panner.pan.value = this.value;	
+// }, false);
 
 // connect our graph
-track.connect(gainNode).connect(panner).connect(audioCtx.destination);
+// track.connect(gainNode).connect(panner).connect(audioCtx.destination);
+// track.connect(audioCtx.destination);
 
-const powerButton = document.querySelector('.control-power');
+// const powerButton = document.querySelector('.control-power');
 
-powerButton.addEventListener('click', function() {
-	if (this.dataset.power === 'on') {
-		audioCtx.suspend();
-		this.dataset.power = 'off';
-	} else if (this.dataset.power === 'off') {
-		audioCtx.resume();
-		this.dataset.power = 'on';
-	}
-	this.setAttribute( "aria-checked", state ? "false" : "true" );
-	console.log(audioCtx.state);
-}, false);
+// powerButton.addEventListener('click', function() {
+// 	if (this.dataset.power === 'on') {
+// 		audioCtx.suspend();
+// 		this.dataset.power = 'off';
+// 	} else if (this.dataset.power === 'off') {
+// 		audioCtx.resume();
+// 		this.dataset.power = 'on';
+// 	}
+// 	this.setAttribute( "aria-checked", state ? "false" : "true" );
+// 	console.log(audioCtx.state);
+// }, false);
 
 // Track credit: Outfoxing the Fox by Kevin MacLeod under Creative Commons 
 
