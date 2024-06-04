1b3aff32af6bc0482697c734d98541c3d6dc4b8b
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-04 12:05:40 -0400

    oneShots can now be played from existing buffers

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ class AudioEngine {
   constructor() {
     this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     this.loops = {}
+    this.oneShotBuffers = {}
+    this.oneShots = {}
+  }
+
+  async playOneShot(name, volume){
+    let bufferSource = this.audioCtx.createBufferSource();
+    bufferSource.buffer = this.oneShotBuffers[name]
+
+    let gainNode = this.audioCtx.createGain();
+    gainNode.gain.value = volume
+
+    bufferSource.connect(gainNode).connect(this.audioCtx.destination);
+    bufferSource.start()
+  }
+
+  async createOneShot(name, url) {
+    this.oneShotBuffers[name] = await this.loadAudioFile(url)
   }
 
   async loadAudioFile(url) {
@@ -27,9 +44,7 @@ class AudioEngine {
   }
 
   async activateContext(){
-    if (this.audioCtx.state === 'suspended') {
-      this.audioCtx.resume();
-    }
+    if (this.audioCtx.state === 'suspended') this.audioCtx.resume();
   }
 
   async loopVolume(loopName, volume, additive = false){
@@ -37,7 +52,7 @@ class AudioEngine {
       return
     }
 
-    if (additive)volume += this.loops[loopName].gain.gain.value
+    if (additive) volume += this.loops[loopName].gain.gain.value
 
     const gainNode = this.loops[loopName].gain
     gainNode.gain.value = volume
@@ -49,9 +64,12 @@ const speakers = new AudioEngine();
 
 speakers.activateContext()
 speakers.createLoop('cicadas', "sfx/cicadas.wav")
+speakers.createOneShot('squeak', 'sfx/squeak1.m4a')
 
 document.addEventListener("keyup", function(event){
 
-  if (event.key === "s")speakers.loopVolume('cicadas', .5, true)
+  if (event.key === "s") speakers.loopVolume('cicadas', .5, true)
+
+  if (event.key === "j") speakers.playOneShot("squeak", .5)
 
 })
