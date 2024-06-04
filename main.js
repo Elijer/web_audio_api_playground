5474b0239c4e1e376b67deff3c62c9e78c2c4164
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-04 11:22:40 -0400

    added a loop volume method

--- main.js
+++ main.js
@@ -13,25 +13,45 @@ class AudioEngine {
   async createLoop(name, url){
     let bufferSource = this.audioCtx.createBufferSource();
     bufferSource.buffer = await this.loadAudioFile(url)
-    bufferSource.connect(this.audioCtx.destination);
+
+    let gainNode = this.audioCtx.createGain();
+    gainNode.gain.value = .06
+
+    bufferSource.connect(gainNode).connect(this.audioCtx.destination);
     bufferSource.loop = true;
     bufferSource.start();
-    this.loops[name] = bufferSource
+    this.loops[name] = {
+      source: bufferSource,
+      gain: gainNode
+    }
   }
 
-  async activate(){
+  async activateContext(){
     if (this.audioCtx.state === 'suspended') {
       this.audioCtx.resume();
     }
   }
 
+  async loopVolume(loopName, volume, additive = false){
+    if (!this.loops[loopName]){
+      return
+    }
+
+    if (additive)volume += this.loops[loopName].gain.gain.value
+
+    const gainNode = this.loops[loopName].gain
+    gainNode.gain.value = volume
+    console.log(gainNode)
+  }
 }
 
 const speakers = new AudioEngine();
 
+speakers.activateContext()
+speakers.createLoop('cicadas', "sfx/cicadas.wav")
+
 document.addEventListener("keyup", function(event){
-  if (event.key === "f"){
-    speakers.activate()
-    speakers.createLoop('cicadas', "sfx/cicadas.wav")
-  }
+
+  if (event.key === "s")speakers.loopVolume('cicadas', .5, true)
+
 })
